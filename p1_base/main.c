106134597220d2dd6a0cf292e32b7bad6b1fa791
Spokiris <112871136+Spokiris@users.noreply.github.com>
2023-11-21 16:09:55 +0000

    ex1 new SOL

--- p1_base/main.c
+++ p1_base/main.c
@@ -3,48 +3,44 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include <dirent.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+
 #include "constants.h"
 #include "operations.h"
 #include "parser.h"
 #include <sched.h>
 
-int main(int argc, char *argv[])
-{
-  unsigned int state_access_delay_ms = STATE_ACCESS_DELAY_MS;
-
-  if (argc > 1)
-  {
-    char *endptr;
-    unsigned long int delay = strtoul(argv[1], &endptr, 10);
-
-    if (*endptr != '\0' || delay > UINT_MAX)
-    {
-      fprintf(stderr, "Invalid delay value or value too large\n");
-      return 1;
-    }
-
-    state_access_delay_ms = (unsigned int)delay;
-  }
-
-  if (ems_init(state_access_delay_ms))
+/*
+  execute_coms(fd, delay) if fd == 0 standart input/output is used
+*/
+int execute_coms(int fd, unsigned int delay) {
+  
+  if (ems_init(delay))
   {
     fprintf(stderr, "Failed to initialize EMS\n");
     return 1;
   }
-
+  
   while (1)
   {
-    unsigned int event_id, delay;
+    
+    unsigned int event_id;
+    
     size_t num_rows, num_columns, num_coords;
     size_t xs[MAX_RESERVATION_SIZE], ys[MAX_RESERVATION_SIZE];
 
     printf("> ");
     fflush(stdout);
 
-    switch (get_next(STDIN_FILENO))
+    switch (get_next(fd))
     {
     case CMD_CREATE:
-      if (parse_create(STDIN_FILENO, &event_id, &num_rows, &num_columns) != 0)
+      if (parse_create(fd, &event_id, &num_rows, &num_columns) != 0)
       {
         fprintf(stderr, "Invalid command. See HELP for usage\n");
         continue;
@@ -58,7 +54,7 @@ int main(int argc, char *argv[])
       break;
 
     case CMD_RESERVE:
-      num_coords = parse_reserve(STDIN_FILENO, MAX_RESERVATION_SIZE, &event_id, xs, ys);
+      num_coords = parse_reserve(fd, MAX_RESERVATION_SIZE, &event_id, xs, ys);
 
       if (num_coords == 0)
       {
@@ -74,7 +70,7 @@ int main(int argc, char *argv[])
       break;
 
     case CMD_SHOW:
-      if (parse_show(STDIN_FILENO, &event_id) != 0)
+      if (parse_show(fd, &event_id) != 0)
       {
         fprintf(stderr, "Invalid command. See HELP for usage\n");
         continue;
@@ -96,7 +92,7 @@ int main(int argc, char *argv[])
       break;
 
     case CMD_WAIT:
-      if (parse_wait(STDIN_FILENO, &delay, NULL) == -1)
+      if (parse_wait(fd, &delay, NULL) == -1)
       { // thread_id is not implemented
         fprintf(stderr, "Invalid command. See HELP for usage\n");
         continue;
@@ -137,3 +133,68 @@ int main(int argc, char *argv[])
     }
   }
 }
+
+unsigned int parse_delay(char *argv)
+{   
+  unsigned int state_access_delay_ms = STATE_ACCESS_DELAY_MS;
+  char *endptr;
+  unsigned long int delay = strtoul(argv, &endptr, 10);
+
+  if (*endptr != '\0' || delay > UINT_MAX)
+  {
+    fprintf(stderr, "Invalid delay value or value too large\n");
+    return 1;
+  }
+
+    state_access_delay_ms = (unsigned int)delay;
+    return state_access_delay_ms;
+}
+
+/*
+main parses just the input
+*/
+int main(int argc, char *argv[])
+{
+  unsigned int delay = parse_delay(argv[1]);
+  if(argc == 2)
+  {
+    execute_coms(0, delay); // executes program w/ parsed delay and standart input/output
+  }
+
+  if (argc > 2) // second arg is a PATH
+  {
+        DIR *currDir;
+        struct dirent *entry;
+
+        currDir = opendir(argv[2]); // open argv[2] directory
+
+        if (currDir == NULL) {
+            perror("Unable to read directory");
+            return 1;
+        }
+
+        while ((entry = readdir(currDir)) != NULL) {
+            
+            char *ext = strrchr(entry->d_name, '.');
+            if (ext != NULL && strcmp(ext, ".jobs") == 0) {
+                
+                int fd = open(entry->d_name, O_RDONLY);
+                if (fd < 0) {
+                    perror("open error");
+                    return -1;
+                }
+                //create.out file FIXME
+                execute_coms(fd, delay); // executes program w/ parsed delay and file input/output
+
+                close(fd);
+            }
+        }
+
+        closedir(currDir);
+    }
+    if(argc == 1)
+    {
+      execute_coms(0, 0);
+    }
+}
+
