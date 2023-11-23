10203058f833bfb56154b48fb2d8181973b10b3f
Spokiris <112871136+Spokiris@users.noreply.github.com>
2023-11-23 04:14:22 +0000

    Formated comments

--- p1_base/main.c
+++ p1_base/main.c
@@ -23,18 +23,19 @@
 #include "parser.h"
 
 #define PATH_MAX 4096
-#define DT_REG 8
+#define DT_REG 8      // Directory entry is a regular file
 
 int main(int argc, char * argv[]) {
-    int fdin = 0;
-    int fdout = 0;
+    int fdin = 0;     // default to stdin -> to be used in FILESYS CMD_SHOW
+    int fdout = 0;    // default to stdout -> to be used in FILESYS CMD_SHOW
 
-    unsigned int state_access_delay_ms = STATE_ACCESS_DELAY_MS;
-    // int MAX_PROC = atoi(argv[1]);
-    DIR * dir;
-    struct dirent * entry;
-    const char * directory_path;
-
+    unsigned int state_access_delay_ms = STATE_ACCESS_DELAY_MS; // Default delay value
+    
+    DIR * dir;                      // Directory stream
+    struct dirent * entry;          // Directory entry
+    const char * directory_path;    // Directory path
+    
+    /* First argument is a FilePath */
     if (argc > 1) {
         directory_path = argv[1];
         dir = opendir(directory_path);
@@ -43,171 +44,194 @@ int main(int argc, char * argv[]) {
             perror("Invalid directory\n");
             return 1;
         }
-
     }
-
-    if (argc > 2) {
-        char * endptr;
-        unsigned long int delay = strtoul(argv[2], & endptr, 10);
+    
+    /* Third argument is the Delay */
+    if (argc > 3) {
+        int MAX_PROC = atoi(argv[2]);                   // Second argument is the  MAX number of simultaneos processes
+        
+        char * endptr;                                  // Pointer to the end of the string
+        unsigned long int delay = strtoul(argv[3], & endptr, 10); 
 
         if ( * endptr != '\0' || delay > UINT_MAX) {
             fprintf(stderr, "Invalid delay value or value too large\n");
             return 1;
         }
 
-        state_access_delay_ms = (unsigned int) delay;
+        state_access_delay_ms = (unsigned int) delay;   // Convert delay to unsigned int
     }
-
+    
+    /* Warning to abusive usage */
+    if (argc > 4) {
+        fprintf(stderr, "Usage: %s <directory_path> <MAX_PROC> <delay>\n", argv[0]);
+        return 1;
+    }
+    
+    /* Initialize EMS */
     if (ems_init(state_access_delay_ms)) {
         fprintf(stderr, "Failed to initialize EMS\n");
         return 1;
     }
 
+    /* Main loop */
     while (1) {
         unsigned int event_id, delay;
         size_t num_rows, num_columns, num_coords;
         size_t xs[MAX_RESERVATION_SIZE], ys[MAX_RESERVATION_SIZE];
         
-        while ((entry = readdir(dir)) != NULL) {
-            if (entry -> d_type == DT_REG) {
+        /* File loop only available w/args */
+        if (argc > 1) {
+                /* Read files in directory */
+            while ((entry = readdir(dir)) != NULL) {
+                
+              if (entry -> d_type == DT_REG) {
 
-                const char * filename = entry -> d_name;
+                  const char * filename = entry -> d_name;
 
-                if (strstr(filename, ".jobs") != NULL) {
-                    char input_path[PATH_MAX];
-                    char output_path[PATH_MAX];
+                  if (strstr(filename, ".jobs") != NULL) {
+                      
+                      char input_path[PATH_MAX];    // Input file path
+                      char output_path[PATH_MAX];   // Output file path
 
-                    snprintf(input_path, PATH_MAX, "%s/%s", directory_path, filename);
+                      snprintf(input_path, PATH_MAX, "%s/%s", directory_path, filename);    // Create input file path
 
-                    fdin = open(input_path, O_RDONLY);
-                    
-                    if (fdin == -1) {
-                        perror("Failed to open file\n");
-                        return 1;
-                    }
+                      fdin = open(input_path, O_RDONLY);    // Open input file descriptor
+                      
+                      if (fdin == -1) {
+                          perror("Failed to open file\n");
+                          return 1;
+                      }
 
-                    int cmd;
-                    while ((cmd = get_next(fdin)) != EOC) {
-                        
-                        switch (cmd) {
+                      int cmd;    // Command number
+                      
+                      /* Loop to keep reading the file until the end*/
+                      while ((cmd = get_next(fdin)) != EOC) {
+                          
+                          switch (cmd) {
 
-                        case CMD_CREATE:
-                            if (parse_create(fdin, & event_id, & num_rows, & num_columns) != 0) {
-                                fprintf(stderr, "Invalid command. See HELP for usage\n");
-                                continue;
-                            }
+                          case CMD_CREATE:
+                              if (parse_create(fdin, & event_id, & num_rows, & num_columns) != 0) {
+                                  fprintf(stderr, "Invalid command. See HELP for usage\n");
+                                  continue;
+                              }
 
-                            if (ems_create(event_id, num_rows, num_columns)) {
-                                fprintf(stderr, "Failed to create event\n");
-                            }
+                              if (ems_create(event_id, num_rows, num_columns)) {
+                                  fprintf(stderr, "Failed to create event\n");
+                              }
 
-                            break;
+                              break;
 
-                        case CMD_RESERVE:
-                            num_coords = parse_reserve(fdin, MAX_RESERVATION_SIZE, & event_id, xs, ys);
+                          case CMD_RESERVE:
+                              num_coords = parse_reserve(fdin, MAX_RESERVATION_SIZE, & event_id, xs, ys);
 
-                            if (num_coords == 0) {
-                                fprintf(stderr, "Invalid command. See HELP for usage\n");
-                                continue;
-                            }
+                              if (num_coords == 0) {
+                                  fprintf(stderr, "Invalid command. See HELP for usage\n");
+                                  continue;
+                              }
 
-                            if (ems_reserve(event_id, num_coords, xs, ys)) {
-                                fprintf(stderr, "Failed to reserve seats\n");
-                            }
+                              if (ems_reserve(event_id, num_coords, xs, ys)) {
+                                  fprintf(stderr, "Failed to reserve seats\n");
+                              }
 
-                            break;
+                              break;
+                          
+                          /* FILESYS CMD_SHOW */
+                          case CMD_SHOW:
+                              if (parse_show(fdin, & event_id) != 0) {    // Parse the command    
+                                  fprintf(stderr, "Invalid command. See HELP for usage\n");
+                                  continue;
+                              }
 
-                        case CMD_SHOW:
-                            if (parse_show(fdin, & event_id) != 0) {
-                                fprintf(stderr, "Invalid command. See HELP for usage\n");
-                                continue;
-                            }
+                              char * dot = strrchr(filename, '.');    // Find the last dot in the filename
+                              if (dot != NULL) {
+                                  * dot = '\0';                       // Terminate the string at the dot
+                              }
 
-                            char * dot = strrchr(filename, '.');
-                            if (dot != NULL) {
-                                * dot = '\0'; // Terminate the string at the dot
-                            }
+                              snprintf(output_path, PATH_MAX, "%s/%s.out", directory_path, filename);   // Create output file path
 
-                            snprintf(output_path, PATH_MAX, "%s/%s.out", directory_path, filename);
+                              fdout = open(output_path, O_WRONLY | O_CREAT | O_TRUNC, 0666);            // Open output file file descriptor
+                              
+                              if (fdout == -1) {
+                                  perror("Failed to open output file");
+                                  return 1;
+                              }
 
-                            fdout = open(output_path, O_WRONLY | O_CREAT | O_TRUNC, 0666);
-                            if (fdout == -1) {
-                                perror("Failed to open output file");
-                                return 1;
-                            }
+                              int stdout_save = dup(STDOUT_FILENO);   // Save standart output file descriptor
+                              dup2(fdout, STDOUT_FILENO);             // Redirect standart output to "xpto.out"
 
-                            int stdout_save = dup(STDOUT_FILENO);
-                            dup2(fdout, STDOUT_FILENO);
+                              if (ems_show(event_id)) {              // EMS_Show printed to "xpto.out"                
+                                  fprintf(stderr, "Failed to show event\n");
+                              }
 
-                            if (ems_show(event_id)) {
-                                fprintf(stderr, "Failed to show event\n");
-                            }
+                              dup2(stdout_save, STDOUT_FILENO);       // Restore stdout file descriptor
 
-                            dup2(stdout_save, STDOUT_FILENO);
+                              if (fsync(fdout) == -1) {               // Force File Sync to "xpto.out"
+                                  perror("Failed to fsync");
+                                  return 1;
+                              }
 
-                            if (fsync(fdout) == -1) {
-                                perror("Failed to fsync");
-                                return 1;
-                            }
+                              close(stdout_save);                     // Close standart output file descriptor
+                              close(fdout);                           // Close "xpto.out" file descriptor
+                              break;
 
-                            close(stdout_save);
-                            close(fdout);
-                            break;
+                          case CMD_LIST_EVENTS:
+                              if (ems_list_events()) {
+                                  fprintf(stderr, "Failed to list events\n");
+                              }
 
-                        case CMD_LIST_EVENTS:
-                            if (ems_list_events()) {
-                                fprintf(stderr, "Failed to list events\n");
-                            }
+                              break;
 
-                            break;
+                          case CMD_WAIT:
+                              if (parse_wait(fdin, & delay, NULL) == -1) { // thread_id is not implemented
+                                  fprintf(stderr, "Invalid command. See HELP for usage\n");
+                                  continue;
+                              }
 
-                        case CMD_WAIT:
-                            if (parse_wait(fdin, & delay, NULL) == -1) { // thread_id is not implemented
-                                fprintf(stderr, "Invalid command. See HELP for usage\n");
-                                continue;
-                            }
+                              if (delay > 0) {
+                                  printf("Waiting...\n");
+                                  ems_wait(delay);
+                              }
 
-                            if (delay > 0) {
-                                printf("Waiting...\n");
-                                ems_wait(delay);
-                            }
+                              break;
 
-                            break;
+                          case CMD_INVALID:
+                              fprintf(stderr, "Invalid command. See HELP for usage\n");
+                              break;
 
-                        case CMD_INVALID:
-                            fprintf(stderr, "Invalid command. See HELP for usage\n");
-                            break;
+                          case CMD_HELP:
+                              printf(
+                                  "Available commands:\n"
+                                  "  CREATE <event_id> <num_rows> <num_columns>\n"
+                                  "  RESERVE <event_id> [(<x1>,<y1>) (<x2>,<y2>) ...]\n"
+                                  "  SHOW <event_id>\n"
+                                  "  LIST\n"
+                                  "  WAIT <delay_ms> [thread_id]\n" // thread_id is not implemented
+                                  "  BARRIER\n" // Not implemented
+                                  "  HELP\n");
 
-                        case CMD_HELP:
-                            printf(
-                                "Available commands:\n"
-                                "  CREATE <event_id> <num_rows> <num_columns>\n"
-                                "  RESERVE <event_id> [(<x1>,<y1>) (<x2>,<y2>) ...]\n"
-                                "  SHOW <event_id>\n"
-                                "  LIST\n"
-                                "  WAIT <delay_ms> [thread_id]\n" // thread_id is not implemented
-                                "  BARRIER\n" // Not implemented
-                                "  HELP\n");
+                              break;
 
-                            break;
+                          case CMD_BARRIER: // Not implemented
+                          case CMD_EMPTY:
+                              break;
 
-                        case CMD_BARRIER: // Not implemented
-                        case CMD_EMPTY:
-                            break;
+                          case EOC:
+                              ems_terminate();
+                              return 0;
+                          }
 
-                        case EOC:
-                            ems_terminate();
-                            return 0;
-                        }
+                          
 
-                        
-
-                    }
-                }
-                close(fdin);
-            }
+                      }
+                  }
+                  close(fdin);
+              }
+          }
+          closedir(dir);
         }
-        closedir(dir);
+        
+        
+        /* Interactive terminal loop (maybe FIXME?) */
         printf("> ");
         fflush(stdout);
 
