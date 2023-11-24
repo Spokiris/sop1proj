6e9bfc8028e0e14630b26ad47c1d58854c074ae5
mc8mac <marcos.machado@tecnico.ulisboa.pt>
2023-11-24 12:08:14 +0000

    added infinite .jobs files

--- p1_base/jobsCreator.py
+++ p1_base/jobsCreator.py
@@ -1,16 +1,5 @@
-# Run me on the ./p1_base folder
-#
-def setContent(n):
-    return f"CREATE {n} 10 10\nRESERVE {n} [(1,1) (10,10)]\nSHOW {n}\n"
-
-def createJobs():
-    for i in range(4,26):
-        filename = f"{chr(i+ord('a'))}.jobs"
-        with open(f"./jobs/{filename}", 'w') as f:
-            f.write(setContent(i+2))
-    return 0
-
 if __name__ == "__main__":
-    
-    createJobs()
-    print("Done!")
+    for i in range(1,1001):
+        f = open(f"./jobs/{i}.jobs", 'w')
+        f.write(f"CREATE {i} 50 50\nRESERVE {i} [(1,1) (50,50)]\nSHOW {i}\n")
+        f.close()
