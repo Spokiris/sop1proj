28217a44b7961b31988cba317b72f57b1081565e
Spokiris <112871136+Spokiris@users.noreply.github.com>
2023-11-24 12:58:32 +0000

    new benchmark

--- p1_base/benchmark.sh
+++ p1_base/benchmark.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# Loop from 1 to 50
-for i in {1..20}
-do
-    echo "Running test $i"
-    (/usr/bin/time -f "Time: %e" ./ems ./jobs $i) 2>> time_results.txt
+# Loop para executar o comando principal para x de 1 a 20
+for ((x = 1; x <= 20; x++)); do
+    make
+    echo "Executando para x = $x"
+    
+    # Comando time para medir o tempo de execução e salvar a saída em um arquivo
+    { time ./ems ./jobs $x; } 2>> benchmark_output.txt
+    
+    echo "--------------------------------------"
+    clear
+    make clean
 done
-
-echo "Test results:"
-cat time_results.txt
